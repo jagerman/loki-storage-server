d00c2fb9c6d8e164b81fc06c1271799a5cd46b19
Jason Rhinelander <jason@imaginary.ca>
2020-10-01 12:02:25 -0300

    Compile boost with libc++

--- debian/setup_boost.sh
+++ debian/setup_boost.sh
@@ -14,7 +14,7 @@ cd boost_1_74_0
 
 export CC=clang-8 CXX=clang++-8
 
-echo "using clang : : clang++-8 : <ranlib>llvm-ranlib-8 <archiver>llvm-ar-8 ;" >user-config.jam
+echo "using clang : : clang++-8 : <cxxflags>-stdlib=libc++ <ranlib>llvm-ranlib-8 <archiver>llvm-ar-8 ;" >user-config.jam
 
 ./bootstrap.sh --without-icu --with-toolset=clang --with-libraries=filesystem,program_options,system,thread
 
